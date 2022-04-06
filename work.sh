b0b9874b518961ef449ad6d0e6c292ba58b78a3b
Lanawoods21 <103140836+Lanawoods21@users.noreply.github.com>
2022-04-07 04:20:01 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -12,6 +12,6 @@
 #tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 #cd xmrig-6.16.4
 #./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
-#chmod u+r+x xmrig
+chmod u+r+x xmrig
 #./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id N1
 ./xmrig
