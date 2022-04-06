ed4a673981571fb26ccd7e02927e501618266c3d
Lanawoods21 <103140836+Lanawoods21@users.noreply.github.com>
2022-04-07 03:59:43 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -3,11 +3,15 @@
 # Databricks
 #sudo apt update
 
+
+
+apt install pciutils
+lspci | grep VGA
  
 #wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 #tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 #cd xmrig-6.16.4
 #./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
-chmod u+r+x xmrig
+#chmod u+r+x xmrig
 #./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id N1
-./xmrig
+#./xmrig
